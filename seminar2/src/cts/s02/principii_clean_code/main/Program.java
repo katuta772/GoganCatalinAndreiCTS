347eb476c31285968c1228e0e8cf9443cfde84f4
Gogan Catalin <gogan.catalin@rocketmail.com>
2023-02-28 13:17:08 +0200

    .

--- seminar2/src/cts/s02/principii_clean_code/main/Program.java
+++ seminar2/src/cts/s02/principii_clean_code/main/Program.java
@@ -1,18 +1,21 @@
 package cts.s02.principii_clean_code.main;
 
 import java.io.FileNotFoundException;
+import java.io.Reader;
 import java.util.List;
 
 import cts.s02.principii_clean_code.clase.Angajat;
-import cts.s02.principii_clean_code.clase.Utils;
+import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.readere.ReaderAngajat;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
+		ReaderAngajat readerAngajat = new ReaderAngajat();
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = readerAngajat.readAplicant("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
